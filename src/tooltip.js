6535091611d081c8703d0fcee8147d75497bafb9
Alejandro Gomez <alexserverone@gmail.com>
2018-12-13 18:14:50 -0600

    Updating tooltip look n style. This is to have a white light look.

--- src/tooltip.js
+++ src/tooltip.js
@@ -7,48 +7,58 @@ function positionTooltip(event, anchor, tooltip) {
 
     // Keep the tooltip near where the mouse is
     var mousePos = {x: event.pageX, y: event.pageY};
-
     var tooltipWidth = tooltip.offsetWidth;
     var tooltipHeight = tooltip.offsetHeight;
 
     var top = mousePos.y - tooltip.offsetHeight - 5;
-    var left = mousePos.x - 5;
+    var left = mousePos.x - 20;
+    var tooltipHorizontalPos = 'ractive-tooltip-left';
+    var tooltipVerticalPos = 'ractive-tooltip-top';
 
-    //if(mousePos.x > win.innerWidth*0.75)
-        //left -= tooltipWidth;
+    // if(mousePos.x > win.innerWidth*0.75)
+    //     left -= tooltipWidth;
 
     // account for the edges of the screen, no need to do left
     var topClip = top - tooltipHeight - 5;
     var rightClip = left + tooltipWidth - win.innerWidth;
 
-    if(rightClip > 0)
-        left -= tooltipWidth - 5;
 
-    // if(topClip < 0)
-    //     top += tooltipHeight*2 - 5;
+    if(rightClip > 0) {
+        left -= tooltipWidth - 38;
+        tooltipHorizontalPos = 'ractive-tooltip-right';
+    }
+
+    if(topClip < 0) {
+        top += tooltipHeight + 15;
+        tooltipVerticalPos = 'ractive-tooltip-bottom';
+    } else
 
     tooltip.style.left = left + 'px';
     tooltip.style.top =  top + 'px';
+    tooltip.className = 'ractive-tooltip ' + tooltipHorizontalPos + ' ' + tooltipVerticalPos;
 }
-
 
 function tooltipDecorator(node, content) {
 
     var tooltip, handlers, eventName;
 
     var start = function(event) {
-
         if(!content || content.length === 0)
             return;
 
         // Create the tooltip
         if(!tooltip)
         {
+            tooltipbody = doc.createElement('div');
+            tooltiptail = doc.createElement('div');
+            tooltipbody.className = 'ractive-tooltip-body';
+            tooltiptail.className = 'ractive-tooltip-tail';
             tooltip = doc.createElement('div');
-            tooltip.className = 'ractive-tooltip';
-            tooltip.textContent = content;
+            tooltip.appendChild(tooltipbody);
+            tooltip.appendChild(tooltiptail);
+            tooltipbody.textContent = content;
+            tooltip.className = 'ractive-tooltip ';
         }
-
         positionTooltip(event, node, tooltip);
 
         doc.body.appendChild(tooltip);
